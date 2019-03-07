d4cd90b486170ea20754167cff82559fb63a8c64
ChungTM1209 <minhchung.ctk17@gmail.com>
2019-03-07 14:20:07 +0700

    create_bill

--- database/migrations/2019_03_07_033957_create_tables_table.php
+++ database/migrations/2019_03_07_033957_create_tables_table.php
@@ -14,7 +14,7 @@ class CreateTablesTable extends Migration
     public function up()
     {
         Schema::create('tables', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');git
             $table->string('name');
             $table->timestamps();
         });
