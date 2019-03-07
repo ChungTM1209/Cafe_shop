df54ba3b1e3449fe252b3a5a33cbf022c053aa0a
duyphamvan <phamduyhvan@gmail.com>
2019-03-07 11:46:40 +0700

    create_product

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,7 +11,7 @@ try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
 
-    require('bootstrap');
+    require('html/Cafe_shop/resources/js/bootstrap');
 } catch (e) {}
 
 /**
