3b0890696f41d6c97b40cd5660b4d3b6f75ecb5d
jinsiko <lequangnghia1997@gmail.com>
2019-03-07 10:55:34 +0700

    create_category

---
+++ database/seeds/CategoryTableSeed.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoryTableSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+            $category = new \App\Category();
+            $category->id = "1";
+            $category->name = "Đồ uống";
+            $category->save();
+
+            $category = new \App\Category();
+            $category->id = "2";
+            $category->name = "Đồ ăn";
+            $category->save();
+
+    }
+}
